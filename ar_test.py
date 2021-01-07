d718dd20e237178d0ff34014998085c32b5ef42f
Tomi <52747351+tomiiha@users.noreply.github.com>
2021-01-07 11:59:38 +0000

    Update ar_test.py

--- ar_test.py
+++ ar_test.py
@@ -1,8 +1,27 @@
 import cv2
+from ipywidgets import interact, Button
 
-capture = cv2.VideoCapture(0)
-while True:
-    ret, image = capture.read()
-    cv2.imshow('Webcam Feed', image)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+window_name = 'Webcam Feed'
+
+# UI bits
+close_button=Button(
+        description='Close Feed',
+        style = {'description_width': 'initial'},
+        disable=False)
+
+cv2.namedWindow(window_name)
+vc = cv2.VideoCapture(0)
+    
+if vc.isOpened(): # Test first frame
+    rval, frame = vc.read()
+else:
+    rval = False
+
+while rval is True:
+    cv2.imshow("View", frame)
+    rval, frame = vc.read()
+    key = cv2.waitKey(20) # T key
+    if key == 27: # ESC to exit process
         break
+vc.release()
+cv2.destroyWindow(window_name)
